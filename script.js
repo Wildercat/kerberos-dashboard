0ccad59f21a141ffcf30e96b025a1707728f24a4
Glitch (garrulous-package) <none>
2019-08-07 23:17:08 +0000

    add scripting to grab data and create html with it

--- script.js
+++ script.js
@@ -3,15 +3,24 @@ $.get( "data", function(data) {
   console.log(data.records[0].fields.Location);
   console.log(data.records.length);
   
+  //create separate divs for each record in the database
   for (let i = 0; i < data.records.length; i++) {
     let newDiv = document.createElement('div');
+    
+    //set id to id from database
     newDiv.setAttribute('id', data.records[i].id);
+    
+    //create header from Location field (from database)
     let location = document.createElement('h2');
     location.innerHTML = data.records[i].fields.Location;
+    newDiv.appendChild(location);
     
+    //create img tag with src from mpegPort field
+    let stream = document.createElement('img');
+    stream.setAttribute('src', '10.0.0.43:' + data.records[i].fields.mpegPort)
+    document.body.appendChild(stream);
     
-    document.body.appendChild(newDiv);
-    
+    //create 
   }
 //  $( "body" )
 //    .append( "Name: " + data.name ) // John
