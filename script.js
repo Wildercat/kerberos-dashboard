9f0f200fd749b894b9bed6acbbf07c515c6342a6
Glitch (garrulous-package) <none>
2019-08-07 23:07:53 +0000

    🐑🌔 Checkpoint ./script.js:5242288/575

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $.get( "data", function(data) {
   console.log(data);
   console.log(data.records[0].fields.Location);
-  console.log(data.records.length)
+  console.log(data.records.length);
   
-//  for (let i = 0; )
+  for (let i = 0; i < data.records.length; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('id', data.records[i].id);
+    let location = document.createElement('h2');
+    location.innerHTML = data.records[i].fields.Location;
+    
+    
+    document.body.appendChild(newDiv);
+    
+  }
 //  $( "body" )
 //    .append( "Name: " + data.name ) // John
 //    .append( "Time: " + data.time ); //  2pm
