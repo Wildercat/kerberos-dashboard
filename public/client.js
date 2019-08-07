a76b8f407d334875bcbc25383ecf8374a565df91
Glitch (garrulous-package) <none>
2019-08-07 00:06:08 +0000

    Import of Wildercat/kerberos-dashboard

--- public/client.js
+++
@@ -1,9 +0,0 @@
-// client-side js
-// run by the browser each time your view template is loaded
-var Airtable = require('airtable');
-var base = new Airtable({apiKey: 'keyvS7t6LbuQP6NZw'}).base('appC3HrtsUWOBFP1R');
-
-base('cameras').find('recWHFyexQ8ADN0Fq', function(err, record) {
-    if (err) { console.error(err); return; }
-    console.log('Retrieved', record.id);
-});
